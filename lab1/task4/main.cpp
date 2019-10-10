ecf70a61205caf89535374757ed79496cc6ee98d
Jan Mazur <mzr4141@gmail.com>
2019-10-10 16:41:34 +0200

    task4 modified

--- lab1/task4/main.cpp
+++ lab1/task4/main.cpp
@@ -4,14 +4,32 @@ enum class Names: uint16_t
 {
     adam,
     anna,
-    marta,
     maciej, // c++11 extra comma
 };
 
+void pozdro600(const std::string &c, Names n){
+    std::cout << c << " ";
+
+    switch(n){
+        case Names::adam:
+            std::cout << "Adam";
+            break;
+
+        case Names::anna:
+            std::cout << "Anna";
+            break;
+
+        case Names::maciej:
+            std::cout << "Maciej";
+            break;
+    }
+    std::cout << "\n";
+
+}
 
 int main(){
-
-
+    
+    pozdro600("Witaj", Names::adam);
 
     return 0;
 }
