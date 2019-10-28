6d9f732c5b25b2c09e32044f41bfca8d3eb91dbe
Jan Mazur <mzr4141@gmail.com>
2019-10-28 15:18:04 +0100

    sfinae?

--- lab3/task4/main.cpp
+++ lab3/task4/main.cpp
@@ -7,12 +7,66 @@
  * konwersji za pomocą cechy typowej is_convertible)
  */
 
+/* obiekt źródłowy może być zadany przez wartość/referencję albo
+ * wskaznik. */
+
 #include <type_traits>
+#include <iostream>
 
+template<class From, class To>
+typename std::enable_if<
+    std::is_pointer<From>::value && 
+    std::is_convertible<
+        From,
+        To
+    >::value 
+>::type
+move(To dst, From src){
+    std::cout <<"pointer\n";     
+    
+};
 
+template<class From, class To>
+typename std::enable_if<
+    !std::is_pointer<From>::value && 
+    std::is_convertible<
+        From,
+        To
+    >::value 
+>::type
+move(To dst, From src){
+    std::cout <<"not pointer\n";     
+
+};
+
+class A {};
+class B : A {};
+
+class C {};
 
 int main(){
+    A *aptr1 = new A();
+    A *aptr2 = new A();
 
+    A aval1;
+    A aval2;
+    A aval3;
+    A aval4;
+
+    B *bptr1 = new B();
+    B *bptr2 = new B();
+    B bval1;
+
+    C *cptr1 = new C();
+
+
+    A &aref1 = aval3;
+
+    move(aval1, aval2); // not pointer
+    move(aval4, aref1); // not pointer
+    move(aptr1, aptr2); // pointer
+
+//    move(bptr1, aptr1);
 
     return 0;
 }
