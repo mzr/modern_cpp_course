e080f68594665c405f7b801f727cd901f1921441
Jan Mazur <mzr4141@gmail.com>
2019-12-19 17:00:09 +0100

    add perms

--- lab9/task3/main.cpp
+++ lab9/task3/main.cpp
@@ -8,6 +8,19 @@
 
 namespace fs = std::filesystem;
 
+void demo_perms(fs::perms p)
+{
+    std::cout << ((p & fs::perms::owner_read) != fs::perms::none ? "r" : "-")
+              << ((p & fs::perms::owner_write) != fs::perms::none ? "w" : "-")
+              << ((p & fs::perms::owner_exec) != fs::perms::none ? "x" : "-")
+              << ((p & fs::perms::group_read) != fs::perms::none ? "r" : "-")
+              << ((p & fs::perms::group_write) != fs::perms::none ? "w" : "-")
+              << ((p & fs::perms::group_exec) != fs::perms::none ? "x" : "-")
+              << ((p & fs::perms::others_read) != fs::perms::none ? "r" : "-")
+              << ((p & fs::perms::others_write) != fs::perms::none ? "w" : "-")
+              << ((p & fs::perms::others_exec) != fs::perms::none ? "x" : "-");
+}
+
 int main(int argc, char **argv){
     if(argc < 3){
         std::cout << "Usage: " 
@@ -31,9 +44,9 @@ int main(int argc, char **argv){
             auto path = p.path();
             auto filename = path.filename();
             if(names.contains(filename)){
-                std::cout << fs::canonical(path) << " " 
-                          << fs::status(path).permissions() << " "
-                          << p.file_size() << std::endl;
+                std::cout << fs::canonical(path) << " ";
+                demo_perms(fs::status(path).permissions());
+                std::cout << " " << p.file_size() << std::endl;
                 //names.erase(filename);
             }
         }
