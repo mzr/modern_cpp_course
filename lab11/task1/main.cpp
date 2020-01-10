baccbd74ed167b6396fe2b7f5689150e54f93045
Jan Mazur <mzr4141@gmail.com>
2020-01-10 14:30:42 +0100

    task1lab11

--- lab11/task1/main.cpp
+++ lab11/task1/main.cpp
@@ -5,3 +5,39 @@
  */
 
 
+/* L(n) = 2                 if n == 0
+ *        1                 if n == 1
+ *        L(n-1) + L(n-2)   if n > 1
+ */
+
+#include <iostream>
+
+constexpr int lucas(int n){
+    if(n == 0)
+        return 2;
+    if(n == 1)
+        return 1;
+    
+    int val0 = 2;
+    int val1 = 1;
+    int val = 0;
+    
+    while(n >= 2){
+        val = val1 + val0;
+        val0 = val1;
+        val1 = val;
+        
+        n--;
+    }
+
+    return val;
+}
+
+int main(){
+    for(int i=0; i<10; i++)
+        std::cout << lucas(i) << " ";
+
+    return 0;
+}
+
+
