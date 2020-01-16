96dd63ba4e003fc38009a6ffd66c270ad7b31e69
Jan Mazur <mzr4141@gmail.com>
2020-01-16 15:27:33 +0100

    task3lab11

--- lab11/task3/main.cpp
+++ lab11/task3/main.cpp
@@ -4,3 +4,24 @@
  * a, b ≥ 1 dla naturalnych liczb 0 ≤ k ≤ n. Funkcja powinna 
  * działać w logarytmicznym czasie O(log a + log b).
  */
+
+#include <cstdint>
+#include <iostream>
+
+template <size_t N, size_t K>
+struct gcd{
+    static constexpr uint64_t val = gcd<K, N % K>::val;
+};
+
+template <size_t N>
+struct gcd<N,0> {
+    static constexpr uint64_t val = N;
+};
+
+
+int main(){
+
+    std::cout << gcd<185, 105>::val;
+
+    return 0;
+}
