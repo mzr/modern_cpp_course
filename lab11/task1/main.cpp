cb68e18d2cbc3612661d456b22b523823c61fac2
Jan Mazur <mzr4141@gmail.com>
2020-01-16 15:00:01 +0100

    lab11task1

--- lab11/task1/main.cpp
+++ lab11/task1/main.cpp
@@ -11,6 +11,23 @@
  */
 
 #include <iostream>
+#include <cstdint>
+
+
+template <intmax_t N>
+struct lucas_num{
+    static constexpr uint64_t val = lucas_num<N-1>::val + lucas_num<N-2>::val;
+};
+
+template <>
+struct lucas_num<0> {
+    static constexpr uint64_t val = 2;
+};
+
+template <>
+struct lucas_num<1> {
+    static constexpr uint64_t val = 1;
+};
 
 constexpr int lucas(int n){
     if(n == 0)
@@ -34,10 +51,16 @@ constexpr int lucas(int n){
 }
 
 int main(){
-    for(int i=0; i<10; i++)
-        std::cout << lucas(i) << " ";
+    std::cout << lucas_num<0>::val << "\n";
+    std::cout << lucas_num<1>::val << "\n";
+    std::cout << lucas_num<2>::val << "\n";
+    std::cout << lucas_num<3>::val << "\n";
+    std::cout << lucas_num<4>::val << "\n";
+    std::cout << lucas_num<5>::val << "\n";
+    std::cout << lucas_num<6>::val << "\n";
+    std::cout << lucas_num<7>::val << "\n";
+    std::cout << lucas_num<8>::val << "\n";
+    std::cout << lucas_num<9>::val << "\n";
 
     return 0;
 }
-
-
